baccdf9363dcb1124ce7f48cbe691ef11dce6991
Ricardo F Tafas Jr <rftafas@repodinamica.com.br>
2020-09-07 11:07:00 -0300

    Removing to test. Some functions will need review due to bogus synth tools.

--- src/std_string.vhd
+++ src/std_string.vhd
@@ -8,50 +8,8 @@ package std_string is
 	type std_logic_string is array (POSITIVE RANGE <> ) of std_logic_vector(7 downto 0);
 	type integer_string   is array (POSITIVE RANGE <> ) of integer range 0 to 255;
 
-	constant string_map : string(1 to 256) :=
-		nul, soh, stx, etx, eot, enq, ack, bel,
-		bs,  ht,  lf,  vt,  ff,  cr,  so,  si,
-		dle, dc1, dc2, dc3, dc4, nak, syn, etb,
-		can, em,  sub, esc, fsp, gsp, rsp, usp,
+	--constant string_map : string(1 to 256) := character'range;
 
-		' ', '!', '"', '#', '$', '%', '&', ''',
-		'(', ')', '*', '+', ',', '-', '.', '/',
-		'0', '1', '2', '3', '4', '5', '6', '7',
-		'8', '9', ':', ';', '<', '=', '>', '?',
-
-		'@', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-		'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-		'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-		'X', 'Y', 'Z', '[', '\', ']', '^', '_',
-
-		'`', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-		'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-		'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-		'x', 'y', 'z', '{', '|', '}', '~', del,
-
-		c128, c129, c130, c131, c132, c133, c134, c135,
-		c136, c137, c138, c139, c140, c141, c142, c143,
-		c144, c145, c146, c147, c148, c149, c150, c151,
-		c152, c153, c154, c155, c156, c157, c158, c159,
-
-		-- the character code for 160 is there (NBSP),
-		-- but prints as no char
-
-		' ', '¡', '¢', '£', '¤', '¥', '¦', '§',
-		'¨', '©', 'ª', '«', '¬',  '­', '®', '¯',
-		'°', '±', '²', '³', '´', 'µ', '¶', '·',
-		'¸', '¹', 'º', '»', '¼', '½', '¾', '¿',
-
-		'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç',
-		'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï',
-		'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', '×',
-		'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'Þ', 'ß',
-
-		'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç',
-		'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï',
-		'ð', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', '÷',
-		'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'þ', 'ÿ'
-	);
 	--VHDL2008 already have functions to convert to string and to hexa_string.
 	--to_string and to_hstring from all 'standard' types.
 	--For GALOIS_VECTOR and for GREY_VECTOR, we can convert to STD_LOGIC_VECTOR first.
@@ -69,7 +27,7 @@ package std_string is
 	function string_match  (l:string; r:string) return boolean;
 	--TO DO: must overload this with all other string types. There will be pain.
 
-	function get_char_code (input:character) return integer;
+	--function get_char_code (input:character) return integer;
 
 end std_string;
 
@@ -114,7 +72,7 @@ package body std_string is
 		end loop;
 
 		if pointer = 0 then
-			report "Could not find %r on string."
+			report "Could not find %r on string.";
 		elsif pointer = 1 then
 			out_tmp(out_tmp'high-2 downto 1) := str_tmp1(str_tmp1'high downto 3) & r;
 		elsif pointer = str_tmp1'high-1 then
@@ -140,14 +98,14 @@ package body std_string is
 		return match;
 	end string_match;
 
-	function get_char_code (input:character) return integer is
-		variable tmp : integer := 0;
-	begin
-			char_loop : for j in string_map'range loop
-				tmp := j;
-				exit char_loop when input = string_map(j);
-			end loop;
-			return j;
-	end get_char_code;
+--	function get_char_code (input:character) return integer is
+--		variable tmp : integer := 0;
+--	begin
+--			char_loop : for j in string_map'range loop
+--				tmp := j;
+--				exit char_loop when input = string_map(j);
+--			end loop;
+--			return j;
+--	end get_char_code;
 
 end std_string;
